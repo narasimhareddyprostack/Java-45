c0461bd5f5a093d0109d79518defa18f64cb2fed
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-11-25 17:44:29 +0530

    okie

---
+++ Lang/Object/Test-toString.java
@@ -0,0 +1,9 @@
+public class Test{
+    public String toString(){
+        return "Rajni";
+    }
+    public static void main(String[] args){
+    Test t = new Test();   
+        System.out.println(t);
+    }
+}
