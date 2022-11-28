6e7039795334b1ef0111aa7693c2a80761b455c1
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-11-28 19:20:56 +0530

    okie

---
+++ CF/Array/Test.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String[] args){
+        int[] ids  = {101,102,103};
+        
+
+        String[] enames = {"Rahul","Priyanka","Sonia"};
+        Object[] objs= {10,"Rahul", 400.5f, 45.5,'C'};
+        objs[10] = "Priyanka";
+    }
+}
---
+++ CF/ArrayListEx/Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Test{
+    public static void main(String[] args){
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add("Thirty");
+        System.out.println(al);
+    }
+}
